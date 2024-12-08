cdb528f7be16b1ae936a1a0a935856ac0d2aa7a5
SenyaM <118691376+SeniaM@users.noreply.github.com>
2024-12-08 13:56:31 +0800

    add del button

--- control.lua
+++ control.lua
@@ -1,21 +1,21 @@
 require "gui"
 
-function tp()
- local player1 = nil
- for _, e in pairs(game.players) do
-  if e.valid and e.name == 'Kin_Chen' then
-   player1 = e
-  end
- end 
- if player1 ~= nil then 
-  local select = player1.selected
-  if select ~= nil then
-   local newpos = {x=select.position.x, y=select.position.y}
-   player1.teleport(newpos, select.surface_index)
-   player1.surface.create_entity{name="my-fire", position=player1.position, force="player"}
-  end
- end
+function tp(player, selected)
+   local newpos = {x=selected.position.x, y=selected.position.y}
+   player.teleport(newpos, selected.surface_index)
+   player.surface.create_entity{name="my-fire", position=player.position, force="player"}
 end
+  
 script.on_event("my-custom-input", function(event)
-    tp()
+    local player = game.get_player(event.player_index)
+    local selected
+    
+    if player.valid and player ~= nil then
+      selected = player.selected
+    end
+    if selected ~= nil then 
+      tp(player, selected)
+    else
+      mainMenu(event)
+    end
 end)
--- gui.lua
+++ gui.lua
@@ -1,4 +1,4 @@
-script.on_event(defines.events.on_player_mined_item, function(event)
+function mainMenu (event)
     if global == nil then global = {} end
     if global.players == nil then global.players = {{}} end
     
@@ -21,27 +21,35 @@ script.on_event(defines.events.on_player_mined_item, function(event)
   content_frame.style.height = 560
 	local controls_flow = content_frame.add{type="flow", name="controls_flow", direction="vertical"}
   controls_flow.style.vertical_spacing = 10
-  local my_table = controls_flow.add{type="table", name = "my_table", column_count= 2}
+  local my_table = controls_flow.add{type="table", name = "my_table", column_count= 3}
   my_table.style.horizontal_spacing = 10
   my_table.style.top_margin = 20
   my_table.style.bottom_margin = 10
   local label1 = my_table.add{type="label", caption="Give a name for the new checkpoint:"}
   local empty_wid = my_table.add{type="empty-widget", style="draggable_space"}
+  local empty_wid2 = my_table.add{type="empty-widget", style="draggable_space"}
   local textfield = my_table.add{type="textfield", name="ugg_controls_textfield"}
   textfield.focus()
+  local delButton = my_table.add{type="button", name="del_button", caption={"test-mod.Del"}, style = "red_button"}
+  delButton.style.font = "default-dialog-button"  
+  delButton.style.width = 80
+  delButton.style.height = 33
   local button = my_table.add{type="button", name="ugg_controls_toggle", caption={"test-mod.Add"}, style = "confirm_button"}
+  button.style.width = 80
   local listbox = controls_flow.add{type = "list-box", name = "ugg_controls_listbox"}
   listbox.style.height = 400
   local exitBtn = controls_flow.add{type= "button", name="ugg_controls_toggle2", caption={"test-mod.tp_exit"}, style = "red_back_button"}
   exitBtn.style.width = 100
   exitBtn.style.horizontal_align = "left"
   exitBtn.style.left_margin = 350
-  exitBtn.style.height = 30
-    for i in pairs(global.players[event.player_index]) do
+  exitBtn.style.height = 33
+    if global.players[event.player_index] ~= nil then 
+      for i in pairs(global.players[event.player_index]) do
           listbox.add_item(i)
+      end
     end
   
-end)
+end
 
 script.on_event(defines.events.on_gui_click, function(event)
     local player = game.get_player(event.player_index)
@@ -54,29 +62,52 @@ script.on_event(defines.events.on_gui_click, function(event)
     elseif event.element.name == "ugg_controls_toggle" then
         local text = player.gui.screen.ugg_main_frame.content_frame.controls_flow.my_table.ugg_controls_textfield
         local list = player.gui.screen.ugg_main_frame.content_frame.controls_flow.ugg_controls_listbox
-        list.add_item(text.text)
-        global.players[event.player_index][text.text] = {x = player.position.x, y = player.position.y}
+        if rawget(global.players, event.player_index) == nil then
+          global.players[event.player_index] = {}
+        end
+        if rawget(global.players[event.player_index], text.text) == nil then
+          list.add_item(text.text)
+          global.players[event.player_index][text.text] = {x = player.position.x, y = player.position.y, surface = player.surface_index}
+        end
+    elseif event.element.name == "del_button" then
+      local list = player.gui.screen.ugg_main_frame.content_frame.controls_flow.ugg_controls_listbox
+      local text = player.gui.screen.ugg_main_frame.content_frame.controls_flow.my_table.ugg_controls_textfield
+      
+      for key, val in ipairs(list.items) do
+        if text.text == val then
+          global.players[event.player_index][text.text] = nil
+          list.remove_item(key)
+        end
+      end
+    --  if text.text == list.items[list.selected_index] then
+    --    global.players[event.player_index][text.text] = nil
+    --   list.remove_item(list.selected_index)
+    --  end
     end
 end)
 
 script.on_event(defines.events.on_gui_selection_state_changed, function(event)
+    local player = game.get_player(event.player_index)
+    local text = player.gui.screen.ugg_main_frame.content_frame.controls_flow.my_table.ugg_controls_textfield
     local serialized_data = serpent.block(global.players)
     settings.global["coords-settings"] = {value = serialized_data}
     game.print(serialized_data)
     if event.element.name == "ugg_controls_listbox" then
       local list = event.element
       local tags = list.items[list.selected_index]
+      text.text = tags
       local coords = global.players[event.player_index][tags]
       local x = coords.x;
       local y = coords.y;
+      local surface = coords.surface;
       game.print(tags)
-      game.get_player(event.player_index).teleport({x, y}, 1)
+      game.get_player(event.player_index).teleport({x, y}, surface)
     end
 end)
 
 
 script.on_init(function()
-  game.print("init")
+  
 end)
 
 script.on_load(function()
@@ -85,5 +116,5 @@ end)
 
 
 script.on_configuration_changed(function(data)
-  game.print("conf_changed")
+  
 end)
